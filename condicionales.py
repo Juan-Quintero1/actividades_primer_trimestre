37a7bc2e1482e729489799a51d1ab88800f3a30b
emanuel <emanuelbanol4@gmail.com>
2025-07-09 17:12:50 -0500

    actividad condicionales

---
+++ condicionales.py
@@ -0,0 +1,85 @@
+# numero1 = int(input("ingrese el numero: "))
+
+# if numero1 > 0:
+#     print(f"es positivo porque es {numero1}")
+# elif numero1 < 0:
+#     print(f"es negativo porque es {numero1}")
+# else:
+#     print(f"es cero {numero1}")
+
+#-------------------------------------------------------------------------------------
+#calcula el mayor de dos numeros ingresados
+
+# numero1 = int(input("ingrese un numero:"))
+# numero2 = int(input("ingrese un numero:"))
+
+# if numero1 > numero2:
+#     print(f"{numero1} es mayor que {numero2}")
+# elif numero1 < numero2:
+#     print(f"{numero1} es menor que {numero2}")
+# else:
+#     print("son iguales")
+
+
+#-------------------------------------ejercicios con condicionales---------------------------------------------------------------------------------
+# 1. verifica si un numero es positivo, negativo o cero
+
+# numero1 = int(input("ingrese el numero: "))
+
+# if numero1 > 0:
+#     print(f"es positivo porque es {numero1}")
+# elif numero1 < 0:
+#     print(f"es negativo porque es {numero1}")
+# else:
+#     print(f"es cero {numero1}")
+
+#----------------------------------------------------------------------------------------------------------------------
+# 2. calcula el mayor de dos numeros ingresados
+
+# numero1 = int(input("ingrese un numero:"))
+# numero2 = int(input("ingrese un numero:"))
+
+# if numero1 > numero2:
+#     print(f"{numero1} es mayor que {numero2}")
+# elif numero1 < numero2:
+#     print(f"{numero1} es menor que {numero2}")
+# else:
+#     print("son iguales")
+
+#-----------------------------------------------------------------------------------------------------------------
+# 3. determina si un numero es par o impar
+
+# numero = int(input("ingresa un numero"))
+
+# if numero % 2 == 0:
+#     print(f"el numero {numero} es par")
+# else:
+#     print(f"el numero {numero} es impar")
+
+#----------------------------------------------------------------------------------------------------------------------------
+# 4. dado un numero verifica si esta entre 10 y 20
+
+# numero = int(input("ingrese un numero:"))
+
+# if numero <=9 or numero >=21:
+#     print("el numero no esta entre 10 y 20")
+# else:
+#     print("el numero esta entre 10 y 20")
+
+#--------------------------------------------------------------------------------------------------------------
+# 5. dado tres numeros, encuentra el mayor usando condicionales
+
+numero1 = int(input("ingrese un numero:"))
+numero2 = int(input("ingrese un numero:"))
+numero3 = int(input("ingrese un numero:"))
+
+if numero1 > numero2 and numero3:
+    print(f"el numero {numero1} es el mayor")
+elif numero2 > numero3 and numero1:
+    print(f"el numero {numero2} es el mayor")
+else:
+    print(f"el numero {numero3} es el mayor")
+
+#-------------------------------------------------------------------------------------------------------------
+# 6. calcula el precio final con un 10% de descuento es mayor a 100.
+
